8f23d165cf3fa4965fc6206bdd3c9fd60d655b89
Lucas Bickel <hairmare@purplehaze.ch>
2012-12-02 11:32:20 +0100

    refactored agent submodule

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -2,8 +2,51 @@
 #
 # Manage a zabbix agent
 #
-class zabbix::agent {
-  $template = "zabbix/${zabbix::params::zabbix_agentd_conf_template}"
+class zabbix::agent (
+  $hostname           = $::hostname,
+  $server             = undef,
+  $listen_ip          = undef,
+  $pid_file           = undef,
+  $log_file           = undef,
+  $userparameters     = undef,
+  $agent_include_path = undef) {
+  include zabbix::params
+  $server_ip_real = $server ? {
+    undef   => $zabbix::params::server,
+    default => $server
+  }
+  $listen_ip_real = $listen_ip ? {
+    undef   => $zabbix::params::agent_listen_ip,
+    default => $listen_ip
+  }
+  $pid_file_real  = $pid_file ? {
+    undef   => $zabbix::params::agent_pid_file,
+    default => $pid_file
+  }
+  $log_file_real  = $log_file ? {
+    undef   => $zabbix::params::agent_log_file,
+    default => $log_file
+  }
+  $userparameters_real            = $userparameters ? {
+    undef   => $zabbix::params::agent_userparameters,
+    default => $userparameters
+  }
+  $has_userparameters             = $userparameters_real ? {
+    undef   => false,
+    default => true
+  }
+  $agent_include_path_real        = $agent_include_path ? {
+    undef   => $zabbix::params::agent_include_path,
+    default => $agent_include_path
+  }
+  # compat: define stuff still used in win template
+  $zabbix_server_ip               = $server_ip_real
+  $ipHostNumber   = $listen_ip_real
+  $zabbix_agentd_pid_file         = $pid_file_real
+  $zabbix_agentd_log_file         = $log_file_real
+  $zabbix_supports_userparameters = $has_userparameters
+
+  $template       = "zabbix/${zabbix::params::zabbix_agentd_conf_template}"
 
   file { $zabbix::params::zabbix_agentd_conf_file:
     content => template($template),
@@ -25,3 +68,4 @@ class zabbix::agent {
   }
 
 }
+
