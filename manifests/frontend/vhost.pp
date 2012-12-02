fe3e592999594b005307ba4a6125d39d7564a9ea
Lucas Bickel <hairmare@purplehaze.ch>
2012-12-02 23:22:40 +0100

    simple vhost implementation

--- manifests/frontend/vhost.pp
+++ manifests/frontend/vhost.pp
@@ -1,4 +1,34 @@
 # == Class: zabbix::frontend::vhost
 #
-class zabbix::frontend::vhost ($ensure = undef) {
+class zabbix::frontend::vhost (
+  $ensure  = undef,
+  $docroot = undef,
+  $port    = undef,
+  $host    = undef) {
+  include zabbix::params
+  $ensure_real  = $ensure ? {
+    undef   => $zabbix::params::frontend,
+    default => $ensure
+  }
+  $docroot_real = $docroot ? {
+    undef   => $zabbix::params::frontend_docroot,
+    default => $docroot
+  }
+  $port_real    = $port ? {
+    undef   => $zabbix::params::frontend_port,
+    default => $port
+  }
+  $host_real    = $host ? {
+    undef   => $zabbix::params::frontend_host,
+    default => $host
+  }
+
+  include apache
+
+  apache::vhost { $host_real:
+    vhost_name => $host_real,
+    docroot    => $docroot_real,
+    port       => $port_real,
+  }
+
 }
