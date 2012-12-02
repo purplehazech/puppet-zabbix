dbd46dc1a42d407c8437aac88f6810fe61e68a96
Lucas Bickel <hairmare@purplehaze.ch>
2012-12-02 18:33:07 +0100

    move vhost module out of base frontend class

--- spec/classes/zabbix_frontend_spec.rb
+++ spec/classes/zabbix_frontend_spec.rb
@@ -5,36 +5,29 @@ describe 'zabbix::frontend' do
   context "on gentoo" do
     let(:facts) { 
       {
+        :fqdn            => 'f.q.d.n.example.com',
         :operatingsystem => 'Gentoo',
-        :fqdn => 'f.q.d.n.example.com',
-        :id => 'id'
       }
     }
     let(:params) {
       {
         :ensure  => 'present',
-        :version => '2.0.3'
+        :version => '2.0.3',
       }
     }
     it {
       should contain_class('zabbix::frontend::gentoo').with({
-        :ensure => 'present'
-      })
-      should contain_class('apache').with({
-        :serveradmin => 'id@f.q.d.n.example.com'
-      })
-      should contain_apache__vhost('f.q.d.n.example.com').with({
-        :docroot         => '/var/www/f.q.d.n.example.com/htdocs',
-        :priority        => '10',
-        :vhost_name      => 'f.q.d.n.example.com',
-        :port            => '80',
+        :ensure  => 'present',
       })
       should contain_webapp_config('zabbix').with({
-        :action => 'install', 
-        :vhost => 'f.q.d.n.example.com',
-        :base => '/zabbix', 
-        :app => 'zabbix', 
-        :version => '2.0.3'
+        :action  => 'install', 
+        :vhost   => 'f.q.d.n.example.com',
+        :base    => '/zabbix', 
+        :app     => 'zabbix', 
+        :version => '2.0.3',
+      })
+      should contains_class('zabbix::frontend::vhost').with({
+        :ensure  => 'present',
       })
     }
   end
