a1566ef9a158f9f57a43139306aabda155cb3c80
Lucas Bickel <hairmare@purplehaze.ch>
2012-12-02 23:52:25 +0100

    pass along ensure

--- manifests/frontend/gentoo.pp
+++ manifests/frontend/gentoo.pp
@@ -4,10 +4,13 @@
 #
 class zabbix::frontend::gentoo ($ensure) {
   file { '/etc/portage/package.use/10_zabbix__frontend':
+    ensure  => $ensure,
     content => 'net-analyzer/zabbix ldap mysql frontend snmp'
   } -> file { '/etc/portage/package.use/10_zabbix__frontend_dev_lang_php':
+    ensure  => $ensure,
     content => 'dev-lang/php truetype gd bcmath sockets sysvipc xmlwriter xmlreader'
   } -> file { '/etc/portage/package.use/10_gd-zabbix__frontend_media_libs_gd':
+    ensure  => $ensure,
     content => 'media-libs/gd png'
   }
 }
