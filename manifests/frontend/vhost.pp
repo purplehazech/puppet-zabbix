0d32794809ce1ce48a7463debfa910cdf8e33396
Lucas Bickel <hairmare@purplehaze.ch>
2012-12-03 22:04:45 +0100

    generate docroot on the fly if host overridden

--- manifests/frontend/vhost.pp
+++ manifests/frontend/vhost.pp
@@ -2,17 +2,28 @@
 #
 class zabbix::frontend::vhost (
   $ensure   = undef,
+  $hostname = undef,
   $docroot  = undef,
-  $port     = undef,
-  $hostname = undef) {
+  $port     = undef) {
   include zabbix::params
   $ensure_real = $ensure ? {
     undef   => $zabbix::params::frontend,
     default => $ensure
   }
   validate_re($ensure_real, [absent, present])
+  $hostname_real = $hostname ? {
+    undef   => $zabbix::params::frontend_hostname,
+    default => $hostname
+  }
+  validate_string($hostname_real)
+
+  if $hostname_real == $zabbix::params::frontend_hostname {
+    $config_docroot = $zabbix::params::frontend_docroot
+  } else {
+    $config_docroot = "/var/www/${hostname_real}/htdocs"
+  }
   $docroot_real = $docroot ? {
-    undef   => $zabbix::params::frontend_docroot,
+    undef   => $config_docroot,
     default => $docroot
   }
   validate_absolute_path($docroot_real)
@@ -20,22 +31,18 @@ class zabbix::frontend::vhost (
     undef   => $zabbix::params::frontend_port,
     default => $port
   }
-  $host_real = $hostname ? {
-    undef   => $zabbix::params::frontend_hostname,
-    default => $hostname
-  }
 
   include apache
 
-  apache::vhost { $host_real:
-    vhost_name => $host_real,
+  apache::vhost { $hostname_real:
+    vhost_name => $hostname_real,
     docroot    => $docroot_real,
     port       => $port_real,
     ssl        => false
   }
 
   apache::vhost::include::php { 'zabbix':
-    vhost_name => $host_real,
+    vhost_name => $hostname_real,
     values     => [
       "date.timezone \"${timezone}\"",
       'post_max_size "32M"',
