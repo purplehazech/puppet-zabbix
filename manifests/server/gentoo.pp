4b12cf083960e954f2f974b0dc3c9cb4aff34c2b
Lucas Bickel <hairmare@purplehaze.ch>
2012-12-04 23:32:27 +0100

    depend on package on gentoo

--- manifests/server/gentoo.pp
+++ manifests/server/gentoo.pp
@@ -2,6 +2,7 @@
 #
 class zabbix::server::gentoo ($ensure = undef) {
   file { '/etc/portage/package.use/10_zabbix__server':
-    content => 'net-analyzer/zabbix  ldap mysql server jabber snmp -sqlite';
+    content => 'net-analyzer/zabbix  ldap mysql server jabber snmp -sqlite',
+    before  => Package['zabbix']
   }
 }
