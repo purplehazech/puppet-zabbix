5605cca9249632416182c5afee3abe4d76429f85
Lucas Bickel <hairmare@purplehaze.ch>
2012-12-05 22:51:33 +0100

    first test for 2. go at api

---
+++ spec/unit/puppet/provider/zabbix_template_spec.rb
@@ -0,0 +1,32 @@
+
+describe "zabbix_template" do
+  let(:provider) { Puppet::Type.type(:zabbix_template) }
+  
+  it "should get defined as provider" do
+  
+    resource = Puppet::Type.type(:zabbix_template).new({
+      :name => 'blerghli',
+    })
+    resource.provider.class.to_s.should == "Puppet::Type::Zabbix_template::ProviderRuby"
+  end
+  
+  
+  it "should return false on inexistant templates" do
+    resource = Puppet::Type.type(:zabbix_template).new({
+      :name => 'blerghli',
+    })
+    Puppet.settings[:config]= "#{File.dirname(__FILE__)}/../../../../tests/etc/puppet.conf"
+    false === resource.provider().exists?()
+  end
+  
+  it "should create a template, find it and delete it again" do
+    resource = Puppet::Type.type(:zabbix_template).new({
+      :name => 'blerghli',
+    })
+    Puppet.settings[:config]= "#{File.dirname(__FILE__)}/../../../../tests/etc/puppet.conf"
+    resource.provider().create()
+    true === resource.provider().exists?()
+    resource.provider().destroy()
+    false === resource.provider().exists?()
+  end
+end
