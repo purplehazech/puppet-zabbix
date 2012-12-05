da745f87c8de91d414d13619f3f38b2b160ca2d8
Lucas Bickel <hairmare@purplehaze.ch>
2012-12-05 20:02:30 +0100

    give travis something to do

---
+++ lib/ruby/zabbix.rb
@@ -0,0 +1,22 @@
+
+require "puppet"
+require "zabbixapi"
+
+# moxin for generic zabbix api stuff
+module Zabbix
+  # initialy load config and setup zabbix api
+  def zbx
+    
+    config_file = File.join(File.dirname(Puppet.settings[:config]), "zabbix.yaml")
+    raise(Puppet::ParseError, "Zabbix report config file #{config_file} not readable") unless File.exist?(config_file)
+    config = YAML.load_file(config_file)
+
+    zbx = ZabbixApi.connect(
+      :url => config.fetch('zabbix_url', 'http://localhost/zabbix/api_jsonrpc.php'),
+      :user => config.fetch('zabbix_user','Admin'),
+      :password => config.fetch('zabbix_password', 'zabbix')
+    )
+    return zbx
+  end
+
+end
