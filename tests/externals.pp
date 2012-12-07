39ae4629644a1ecf7ba55b0b5553a18edd46be3b
Lucas Bickel <hairmare@purplehaze.ch>
2012-12-07 13:57:50 +0100

    [ci skip] docs

--- tests/externals.pp
+++ tests/externals.pp
@@ -2,6 +2,12 @@
 #
 # node for testing zabbix::externals
 #
+# === Example Usage
+#
+#   rake spec_prep
+#   puppet apply --noop --modulepath spec/fixtures/modules/ \
+#   tests/externals.pp --trace
+#
 node default {
   class { 'zabbix::externals':
     ensure => 'present',
