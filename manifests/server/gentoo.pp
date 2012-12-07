e341d4d0b9e5538e0a69e0f2959f2a7a214fd1b1
Lucas Bickel <hairmare@purplehaze.ch>
2012-12-07 14:09:48 +0100

    useflags for activerecord mysql

--- manifests/server/gentoo.pp
+++ manifests/server/gentoo.pp
@@ -4,5 +4,8 @@ class zabbix::server::gentoo ($ensure = undef) {
   file { '/etc/portage/package.use/10_zabbix__server':
     content => 'net-analyzer/zabbix  ldap mysql server jabber snmp -sqlite',
     before  => Package['zabbix']
+  } -> file { '/etc/portage/package.use/10_zabbix__server_activerecord':
+    content => 'dev-ruby/activerecord mysql',
+    before  => Package['activerecord']
   }
 }
