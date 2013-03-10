498e4ea14b366bdbe038b51fc9ec531948d69ed2
Lucas Bickel <hairmare@purplehaze.ch>
2013-03-10 21:04:42 +0100

    don't install apache when frontend is not requested

--- manifests/frontend/vhost.pp
+++ manifests/frontend/vhost.pp
@@ -13,22 +13,24 @@ class zabbix::frontend::vhost (
     default => $docroot
   }
   validate_absolute_path($docroot_real)
+  
+  if ($ensure == present) {
+    include apache
 
-  include apache
+    apache::vhost { $hostname:
+      vhost_name => $hostname,
+      docroot    => $docroot_real,
+      port       => $port,
+      ssl        => false
+    }
 
-  apache::vhost { $hostname:
-    vhost_name => $hostname,
-    docroot    => $docroot_real,
-    port       => $port,
-    ssl        => false
-  }
-
-  apache::vhost::include::php { 'zabbix':
-    vhost_name => $hostname,
-    values     => [
-      "date.timezone \"${::timezone}\"",
-      'post_max_size "32M"',
-      'max_execution_time "600"',
-      'max_input_time "600"']
+    apache::vhost::include::php { 'zabbix':
+      vhost_name => $hostname,
+      values     => [
+        "date.timezone \"${::timezone}\"",
+        'post_max_size "32M"',
+        'max_execution_time "600"',
+        'max_input_time "600"']
+    }
   }
 }
