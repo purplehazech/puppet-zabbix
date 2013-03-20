599d76b1c752ed6214395ea7b834abf7a727f5bc
Clemens Bergmann <c.bergmann@proventa.de>
2013-03-20 02:22:22 +0100

    debian zabbixversion

--- lib/facter/zabbixversion.rb
+++ lib/facter/zabbixversion.rb
@@ -1,5 +1,12 @@
 Facter.add("zabbixversion") do
-  setcode do
-    Facter::Util::Resolution.exec('eix --nocolor --pure-packages --format "<installedversions:VERSION>"  --stable --installed --name zabbix')
+  distid = Facter.value('operatingsystem')
+    case distid
+    when "Gentoo"
+      Facter::Util::Resolution.exec('eix --nocolor --pure-packages --format "<installedversions:VERSION>"  --stable --installed --name zabbix')
+    when "Debian"
+      version = Facter::Util::Resolution.exec('dpkg-query -W -f=\'${Version}\' zabbix-agent')
+      array = version.split(":")
+      array[array.size-1]
+    end
   end
 end
