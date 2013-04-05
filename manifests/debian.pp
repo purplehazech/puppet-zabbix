c381365f4bc71e3dc3b5499880f436d180f00ee9
Clemens Bergmann <c.bergmann@proventa.de>
2013-04-05 21:56:01 +0200

    using shorter fingerprint

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -9,7 +9,7 @@ class zabbix::debian () {
     location   => 'http://repo.zabbixzone.com/debian',
     repos      => 'main contrib non-free',
     release    => 'squeeze',
-	key        => '8FF57A0425FFD7E7',
+	key        => '25FFD7E7',
 	key_server => 'keys.gnupg.net'
   }
 }
