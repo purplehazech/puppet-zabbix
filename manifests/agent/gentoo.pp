68d7674fab18168916c570ae01c0464c29108ac0
Lucas Bickel <hairmare@purplehaze.ch>
2014-01-12 15:34:05 +0100

    explicit install of zabbix api
    this seems to be lacking somehow, hopefully feature
    scoping will make this behave as I intend it to. To get
    any further I will need to start refactoring server stuff
    to get a working /etc/puppet/zabbix.api.yaml on dev.

--- manifests/agent/gentoo.pp
+++ manifests/agent/gentoo.pp
@@ -10,6 +10,12 @@ class zabbix::agent::gentoo (
     ensure => present,
     use    => ['agent'],
     target => '10_zabbix__agent'
+  } ->
+  # this gem is a dependency of this module
+  # @todo librarian-puppet should pull this!
+  package { 'zabbixapi':
+    ensure   => present,
+    provider => 'gem',
   }
 
 }
