e13837fddf536559158ce67b6a4cee0fd30734be
Lucas Bickel <hairmare@purplehaze.ch>
2014-01-14 22:54:34 +0100

    fix wrong onlyif syntax

--- manifests/gentoo.pp
+++ manifests/gentoo.pp
@@ -44,63 +44,63 @@ class zabbix::gentoo {
     default => [],
   }
 
-  package { 'zabbixapi':
-    ensure   => present,
-    provider => 'gem',
-    onlyif   => lookup('use_api', 'Boolean'),
+  if (lookup('use_api', 'Boolean')) {
+    package { 'zabbixapi':
+      ensure   => present,
+      provider => 'gem',
+    }
   }
 
   package_use { 'net-analyzer/zabbix':
-    ensure  => true,
+    ensure  => present,
     use     => concat(
-      $default_use_flags,
-      $agent_use_flags,
-      $server_use_flags,
-      $frontend_use_flags
+      concat(
+        $default_use_flags,
+        $agent_use_flags
+      ),
+      concat(
+        $server_use_flags,
+        $frontend_use_flags
+      )
     ),
   }
 
-  package_use { 'dev-lang/php':
-    ensure => true,
-    use    => [
-      'truetype',
-      'gd',
-      'bcmath',
-      'sockets',
-      'sysvipc',
-      'xmlwriter',
-      'xmlreader',
-      'apache2',
-      'mysql',
-      'ldap',
-    ],
-    onlyif => $frontend_defined
-  }
+  if ($frontend_defined) {
+    package_use { 'dev-lang/php':
+      ensure => true,
+      use    => [
+        'truetype',
+        'gd',
+        'bcmath',
+        'sockets',
+        'sysvipc',
+        'xmlwriter',
+        'xmlreader',
+        'apache2',
+        'mysql',
+        'ldap',
+      ],
+    }
 
-  package_use { 'media-libs/gd':
-    ensure => true,
-    use    => [
-      'png',
-    ],
-    onlyif => $frontend_defined
-  }
+    package_use { 'media-libs/gd':
+      ensure => true,
+      use    => [
+        'png',
+      ],
+    }
 
-  # Gentoo uses webapp-config
-  $webapp_action = lookup('frontend_ensure', 'Boolean') ? {
-    present => 'install',
-    absent  => 'remove',
-    default => noop
-  }
-
-  webapp_config { 'zabbix':
-    action  => $webapp_action,
-    vhost   => lookup('frontend_hostname', 'String'),
-    version => lookup('version'),
-    app     => 'zabbix',
-    base    => lookup('frontend_base', 'String'),
-    depends => [],
-    before  => File[lookup('frontend_conf_file', 'String')],
-    onlyif  => $frontend_defined
+    $webapp_host = lookup('frontend_hostname', 'String')
+    $webapp_base = lookup('frontend_base', 'String')
+    webapp { "${webapp_host}::${webapp_base}":
+      appname    => 'zabbix',
+      appversion => lookup('version'),
+      server     => 'apache',
+      user       => 'apache',
+      group      => 'apache',
+      secure     => true,
+      before     => File[lookup('frontend_conf_file', 'String')],
+      onlyif     => $frontend_defined
+    }
   }
 
 }
