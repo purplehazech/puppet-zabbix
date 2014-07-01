129c57cdcab9d1b1297050aad1c7c93c4c36faca
Clemens Bergmann <c.bergmann@proventa.de>
2014-07-01 22:13:54 +0200

    new key source

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -10,8 +10,7 @@ class zabbix::debian () {
     location   => "http://repo.zabbix.com/zabbix/2.2/$diststring",
     repos      => 'main contrib non-free',
     release    => $lsbdistcodename,
-	key        => '79EA5ED4',
-	key_server => 'keys.gnupg.net'
+	  key_source => 'http://repo.zabbix.com/zabbix-official-repo.key'
   }
   
   apt::source { 'zabbixzone':
